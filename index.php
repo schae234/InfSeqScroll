da4b376ab254cb2f064189c847d556dfe40c5640
Rob <schae234@umn.edu>
2012-12-08 16:10:12 -0600

    Organized the main js classes

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html  lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" >
 <head>
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type"></meta>
     <link href="img/favicon.ico" rel="shortcut icon">
+	<script src="js/tools.js" type="text/javascript"></script>
     <script src="js/ReadMapper.js" type="text/javascript"></script>
     <link type="text/css" rel="stylesheet" href="css/main.css"></link>
+
+	<script type="text/javascript">
+		function init(){
+			 rm = new ReadMapper(document.getElementById("ReadMapper"))
+			 var a = 1;
+		}
+	</script>
+
 </head>
 
-<body>
+<body onload="init()">
 <p>Sequence Mapper</p>
-<div class="ReadMapper"></div>
+<div id="ReadMapper"></div>
 </body>
 
 
--- js/ReadMapper.js
+++ js/ReadMapper.js
@@ -1,25 +1,51 @@
+/* Read Mapper
+
+*/
 function ReadMapper(div){
     // Create the sequence frams
     this.seq_frame = new SequenceFrame(div)
     this.search_box = new SearchBox(div)
     this.chromo_map = new ChromoMap(div)
+	
+	this.div = div;
+	this.div.className="ReadMapper"
 
 }
 
-SequReadMapper.prototype = new ReadMapper()
-SequenceFrame.prototype.constructor = SequenceFrame
+ReadMapper.prototype.hello = function(){
+	alert("hello world")
+}
+
+/* Sequence Frame
+
+*/
+
 function SequenceFrame(pdiv){
-
+	// generate the frame
+	this.frame = document.createElement("div")
+	this.frame.className = "SequenceFrame";
+	pdiv.appendChild(this.frame)
 }
+SequenceFrame.prototype = heir(ReadMapper.prototype)
 
-SearchBox = new ReadMapper()
-SearchBox.constructor = SearchBox
+/* Search Box
+
+*/
+
 function SearchBox(pdiv){
 
 }
+SearchBox.prototype = heir(ReadMapper.prototype)
 
-ChromoMap = new ReadMapper()
-Chromomap.constructor = ChromoMap
+SearchBox.prototype.world = function(){
+	alert('goodbye world')
+}
+
+/* Chromo Map
+
+*/
+
 function ChromoMap(pdiv){
 
 }
+ChromoMap.prototype = heir(ReadMapper.prototype)
